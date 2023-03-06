327fcdb7cce631bf349c960fb0250437fe095f4b
Jiatong Han <jiatong.han@u.nus.edu>
2023-03-06 14:40:22 +0800

    Add solver and generator logic

---
+++ Algorithms/generator/data_generator.py
@@ -0,0 +1,3 @@
+"""
+Add boosts to uniform distribution.
+"""
--- Algorithms/solver/lp_solver.py
+++ Algorithms/solver/lp_solver.py
@@ -2,8 +2,21 @@ from ..classes import Agent, ItemState, Allocation
 from ..tools import NOTIONS, get_pareto_efficiency, get_total_utility
 
 
+def find_constraints(allocation: Allocation):
+    '''O(m+2*n) number of constraints: items and each pair of agents (WEF)
+       With fractional allocations, each agent should have no less utlity than others.
+       And item fractions should sum up to one.
+    '''
+    pass
+
+
 class LPSolver:
     def __init__(self, allocation: Allocation):
+        ## Constraints
+
+        ## Objectives (maximize Nash welfare, best Pareto efficiency)
+
+
         # TODO: find constraints, objective functions
         # TODO: implement several LP algorithms and compare performances (converging speed, etc.)
         # TODO: set up a website that enables **real-time** updates and user trials
